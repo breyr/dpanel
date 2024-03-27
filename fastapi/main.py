d10f545db7f0cfe120214e2b04e9b97f20a84eb5
breyr <breyr2021@gmail.com>
2024-03-27 08:57:35 -0400

    add CORS origins

--- fastapi/main.py
+++ fastapi/main.py
@@ -42,7 +42,7 @@ origins = ["http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=origins,  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
