1111781b18ae3ee9cd49453ea3faad0093d8fa0f
adamspera <adamspera@hotmail.com>
2024-03-29 18:32:59 -0400

    Refactor webhook.py to include logging and handle subprocess errors

--- webhook.py
+++ webhook.py
@@ -1,24 +1,30 @@
 from flask import Flask, request
 import subprocess
+import os
+import logging
 
 app = Flask(__name__)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
   event = request.headers.get('X-GitHub-Event')
   # Verify that the request is from GitHub
   if event == 'push':
-    # Stopping and removing existing containers, networks, and volumes
-    subprocess.run(["docker-compose", "down", "--volumes", "--remove-orphans"])
+    try:
+      # Stopping and removing existing containers, networks, and volumes
+      subprocess.check_call(["docker-compose", "down", "--volumes", "--remove-orphans"])
 
-    # Remove existing images (optional, uncomment if needed)
-    # subprocess.run(["docker", "rmi", "$(`docker images -q`)", "-f"])
+      # Pull the latest changes from GitHub
+      subprocess.check_call(["git", "pull", "origin", "--force"])
 
-    # Pull the latest changes from GitHub
-    subprocess.run(["git", "pull", "origin", "--force"])
-
-    # Run docker-compose
-    subprocess.run(["docker-compose", "up", "--build", "-d"])
+      # Run docker-compose
+      subprocess.check_call(["docker-compose", "up", "--build", "-d"])
+    except subprocess.CalledProcessError as e:
+      logging.error(f"Subprocess failed with error {e}")
+      return 'Update and deployment failed', 500
 
     return 'Update and deployment completed successfully', 200
   elif event == 'ping':
@@ -27,4 +33,5 @@ def webhook():
     return 'Unsupported event', 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1470)
+  port = int(os.getenv('PORT', 1470))  # Use environment variable for port
+  app.run(host='0.0.0.0', port=port)
